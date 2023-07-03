4d93c84737a28417f7caac731e318922bab132a3
Wiggidy <rlr0017@auburn.edu>
2023-07-03 14:34:02 -0500

    Fixed "null" registering as invalid Json

--- src/content/JsonUtil.ts
+++ src/content/JsonUtil.ts
@@ -2,24 +2,23 @@ const prettifyObj = (obj: any): string => {
   const objSorted = newObjSorted(obj);
   const jsonPretty = JSON.stringify(objSorted, null, 2);
   return jsonPretty;
-}
+};
 
 const prettifyStr = (str: string): string => {
   const obj = JSON.parse(str);
   return prettifyObj(obj);
-}
+};
 
 // Returns obj with sorted keys if non-array object
 // Otherwise, returns obj as-is
 const newObjSorted = (obj: any) => {
-  if (typeof obj !== 'object' || Array.isArray(obj)) return obj;
-  return Object.keys(obj).sort().reduce((sortedObj: any, key) => {
-    sortedObj[key] = obj[key];
-    return sortedObj;
-  }, {});
-}
-
-export {
-  prettifyObj,
-  prettifyStr,
+  if (typeof obj !== "object" || obj === null || Array.isArray(obj)) return obj;
+  return Object.keys(obj)
+    .sort()
+    .reduce((sortedObj: any, key) => {
+      sortedObj[key] = obj[key];
+      return sortedObj;
+    }, {});
 };
+
+export { prettifyObj, prettifyStr };
